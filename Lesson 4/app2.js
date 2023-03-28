dad1dcf16f6a1ec99ab9f981d94713c86d9d40b3
unknown <yepkaya@gmail.com>
2023-03-28 23:51:29 +0300

    lesson 4

--- Lesson 4/app2.js
+++ Lesson 4/app2.js
@@ -1,14 +1,27 @@
-// TODO console.log ile 
+// // TODO console.log ile 
 
-// document.write ile print
-document.write("Hello world and hi people");
+// // document.write ile print
+// document.write("Hello world and hi people");
 
-document.writeln("selamlar naber <br>")
+// document.writeln("selamlar naber <br>");
 
-document.write("welcome my worl <br>")
+// document.write("welcome my worl <br>");
 
-document.write("selam buradan alt satira gecsin <br> bakalim br sonrasi alt satirda mi<br>");
-document.writeln(true);
-document.writeln(123123123123);
-document.writeln({name:"yusuf",surnama:"purkaya",age :33},"<br>");
-console.log({name:"yusuf",surnama:"purkaya",age :33});
+// document.write("selam buradan alt satira gecsin <br> bakalim br sonrasi alt satirda mi<br>");
+// document.writeln(true);
+// document.writeln(123123123123);
+// document.writeln({name:"yusuf",surnama:"purkaya",age :33},"<br>");
+// console.log({name:"yusuf",surnama:"purkaya",age :33});
+
+let a= 5;
+let b=8;
+
+// console.log(a+b);
+// console.clear;
+console.log(window);
+console.log(window.document.location.host);
+console.clear;
+
+window.alert("Welcome my world");
+window.alert("a ile b nin toplamı : "+(a+b))
+// ardarda 2 adet alert cikacak
