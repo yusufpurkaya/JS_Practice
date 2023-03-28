eb03b5794ff263ec6991e9f306c4d345df0a057d
unknown <yepkaya@gmail.com>
2023-03-28 22:56:37 +0300

    js print

--- Lesson 4/app2.js
+++ Lesson 4/app2.js
@@ -1,6 +1,14 @@
+// TODO console.log ile 
+
 // document.write ile print
 document.write("Hello world and hi people");
 
 document.writeln("selamlar naber <br>")
 
-document.write("welcome my worl")
+document.write("welcome my worl <br>")
+
+document.write("selam buradan alt satira gecsin <br> bakalim br sonrasi alt satirda mi<br>");
+document.writeln(true);
+document.writeln(123123123123);
+document.writeln({name:"yusuf",surnama:"purkaya",age :33},"<br>");
+console.log({name:"yusuf",surnama:"purkaya",age :33});
