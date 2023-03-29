2f99f34685858c665459d24c7f8301ed8262d310
unknown <yepkaya@gmail.com>
2023-03-30 00:22:37 +0300

    lesson 5 and 6

---
+++ Lesson 5-6/L5.js
@@ -0,0 +1,18 @@
+console.log(window);
+// eger bir method window objesinin icindeyse window.  demeye gerek yok
+// direct window icindeki objeleri kullanabiliriz
+
+console.log("host is : ",location.host);
+console.log("port is : ",location.port);
+console.error("console da error olussun");
+console.error("console da error olussun");
+console.error("console da error olussun");
+console.error("console da error olussun");
+debugger;
+console.error("console da error olussun");
+console.error("console da error olussun");
+console.error("console da error olussun");
+debugger;
+console.error("console da error olussun");
+console.error("console da error olussun");
+console.warn("console da warn olussun");
