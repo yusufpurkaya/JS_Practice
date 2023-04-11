e9b16eda0ccab670d32fcb4c07dc396f2781e58c
unknown <yepkaya@gmail.com>
2023-04-11 23:10:10 +0300

    lesson 11

---
+++ Lesson 11/Ll11.js
@@ -0,0 +1,30 @@
+
+document.writeln("Operators types");
+/*
+
+=      assigment operator
++
+-
+*
+/
+%
+++
+--
+**
+
+*/
+
+
+console.log(("+ operator ")+(7+5));
+let a=13;
+let b=14;
+let isim="toplayamaz ";
+console.log(a+b);
+console.log(isim+a+b);  // toplayamaz 1314
+console.log(isim+(a+b)); // toplayamaz 27
+
+
+// - * 
+
+console.log(a*b);
+console.log(a-b);
