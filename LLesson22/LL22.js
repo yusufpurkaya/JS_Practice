b4d376d0942e506c15d3106b6c0a6d5d387acba2
unknown <yepkaya@gmail.com>
2023-04-20 00:26:49 +0300

    lesson 22

---
+++ LLesson22/LL22.js
@@ -0,0 +1,31 @@
+/*
+User Name 
+User TCNo
+User Age
+
+name should not be null
+TCNo should be 11 length
+Age should be higther than 18
+
+if user matches all condition 
+can be premium
+however 
+if user name is empty take name again one more time
+if TCNo doent match our condition take one more time again
+if user is under 18  give a message " come x year later"
+
+and user can not be premium user
+
+
+*/
+
+function userKnowledge(name1,age,TCNo){
+    
+}
+
+
+
+
+
+
+
